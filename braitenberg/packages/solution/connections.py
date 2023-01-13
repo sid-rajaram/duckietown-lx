118912bd8df60ca60962c5c833698963af999a10
sid-rajaram <siddharth.rajaram@gmail.com>
2023-01-13 13:58:51 -0800

    submission 19526

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,6 +7,8 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     res[160:, :320] = np.ones((320,1))*np.linspace(0.05,1,320)[np.newaxis,:]
     # res[160:, 320:] = np.ones((320,1))*np.linspace(-1,-0.05,320)[np.newaxis,:]
+    res[400:, :80] = 1.0
+    res[400:, 560:] = -1.0
     return res
 
 
@@ -14,4 +16,6 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     # res[160:, :320] = np.ones((320,1))*np.linspace(-0.05,-1,320)[np.newaxis,:]
     res[160:, 320:] = np.ones((320,1))*np.linspace(1,0.05,320)[np.newaxis,:]
+    res[400:, :80] = -1.0
+    res[400:, 560:] = 1.0
     return res
