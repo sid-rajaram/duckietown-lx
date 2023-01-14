a1de12f8ad3d988568369b6bc5910cca3bb8e0cd
sid-rajaram <siddharth.rajaram@gmail.com>
2023-01-13 23:39:13 -0800

    solution 19533

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -5,17 +5,17 @@ import numpy as np
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
-    res[160:, :320] = np.ones((320,1))*np.linspace(0.05,1,320)[np.newaxis,:]
-    # res[160:, 320:] = np.ones((320,1))*np.linspace(-1,-0.05,320)[np.newaxis,:]
-    res[400:, :80] = 1.0
-    res[400:, 560:] = -1.0
+    # res[160:, :320] = np.ones((320,1))*np.linspace(0,1,320)[np.newaxis,:]
+    res[160:, 320:] = np.ones((320,1))*np.linspace(-1,0,320)[np.newaxis,:]
+    # res[400:, :80] = 1.0
+    # res[400:, 560:] = -1.0
     return res
 
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
-    # res[160:, :320] = np.ones((320,1))*np.linspace(-0.05,-1,320)[np.newaxis,:]
-    res[160:, 320:] = np.ones((320,1))*np.linspace(1,0.05,320)[np.newaxis,:]
-    res[400:, :80] = -1.0
-    res[400:, 560:] = 1.0
+    res[160:, :320] = np.ones((320,1))*np.linspace(0,-1,320)[np.newaxis,:]
+    # res[160:, 320:] = np.ones((320,1))*np.linspace(1,0,320)[np.newaxis,:]
+    # res[400:, :80] = -1.0
+    # res[400:, 560:] = 1.0
     return res
